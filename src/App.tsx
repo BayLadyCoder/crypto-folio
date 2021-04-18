2ff5c140f9d3ed1d1521973605c3b7bc3f891e8c
BayLadyCoder <rachada_32@hotmail.com>
2021-04-18 01:18:57 -0400

    Added logo and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./images/logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={logo}
+          alt="img"
+          style={{ width: "70px", height: "70px", marginRight: "20px" }}
+        />
+        <h1>Crypto Folio</h1>
       </header>
     </div>
   );
