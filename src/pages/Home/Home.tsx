27ff503904277d3180d6754287890e637821d32a
BayLadyCoder <rachada_32@hotmail.com>
2023-12-07 18:02:08 -0500

    Temporay remove search input on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useContext } from "react";
-import { MarketCoinsContext } from "../../context/MarketCoins/MarketCoinsContext";
-import SearchBar from "../../components/SearchBar";
-import { CryptoTable, CryptoTableName } from "../../components/CryptoTable";
+import { useEffect, useContext } from 'react';
+import { MarketCoinsContext } from '../../context/MarketCoins/MarketCoinsContext';
+import SearchBar from '../../components/SearchBar';
+import { CryptoTable, CryptoTableName } from '../../components/CryptoTable';
 
-import { Container, Loading, PageTitle } from "../../styles/globalStyles";
+import { Container, Loading, PageTitle } from '../../styles/globalStyles';
 const Home = () => {
   const { marketCoins, fetchMarketCoins } = useContext(MarketCoinsContext);
   useEffect(() => {
@@ -15,8 +15,8 @@ const Home = () => {
   return (
     <Container>
       <PageTitle>Market</PageTitle>
-      <SearchBar />
-      <CryptoTableName tableName="Crypto Market Monitor" />
+      {/* <SearchBar /> */}
+      <CryptoTableName tableName='Crypto Market Monitor' />
       <CryptoTable coins={marketCoins} />
     </Container>
   );
