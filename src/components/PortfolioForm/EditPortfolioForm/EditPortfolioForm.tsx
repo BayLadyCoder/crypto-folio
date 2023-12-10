b036d22c4cbd1371336a2d07062aa32d87741980
BayLadyCoder <rachada_32@hotmail.com>
2023-12-10 16:25:41 -0500

    Show real data on edit portfolio and clean up code

--- src/components/PortfolioForm/EditPortfolioForm/EditPortfolioForm.tsx
+++ src/components/PortfolioForm/EditPortfolioForm/EditPortfolioForm.tsx
@@ -1,89 +1,70 @@
-import { useState, useEffect } from "react";
-import { MarketCoin } from "../../../types/coins";
 import {
-  FormContainer,
-  TopContainer,
-  PortfolioNameContainer,
-  PortfolioName,
-  EditIconButton,
-  DetailsContainer,
   BottomContainer,
-  CloseFormButton,
   AddDetailsForm,
-  SelectPortfolioActionsContainer,
   FormTitle,
   Table,
   TableHead,
   TableHeadData,
   TableBody,
   TableBodyData,
-  RemoveButton,
   TableRow,
   RemoveCoinIconButton,
   EditCoinIconButton,
-} from "../PortfolioForm.styled";
-
-import { Button } from "../../../styles/globalStyles";
-import { PortfolioCoinBasic, PortfolioCoin } from "../../../types/coins";
-import {
-  TextFieldWithLabel,
-  SearchCoinTextField,
-  CurrencyRadioButtons,
-} from "../../form_components";
-import { RiAddFill } from "react-icons/ri";
-import { MdModeEdit } from "react-icons/md";
-import AddOrEditCoinsForm from "../AddOrEditCoinForm";
+} from '../PortfolioForm.styled';
+import { Button } from '../../../styles/globalStyles';
+import { usePortfolio } from '../../../context/Portfolio/PortfolioContext';
 
 interface Props {
   setFormStep: any;
 }
 
 const EditPortfolioForm: React.FC<Props> = ({ setFormStep }) => {
+  const { portfolioCoins } = usePortfolio();
+
   const onClickRemoveCoin = (e: React.MouseEvent) => {
     e.preventDefault();
   };
+
+  console.log({ portfolioCoins });
   return (
     <AddDetailsForm>
       <FormTitle>Edit Portfolio</FormTitle>
-      <Table cellSpacing="0" cellPadding="0">
+      <Table cellSpacing='0' cellPadding='0'>
         <TableHead>
           <tr>
-            <TableHeadData align="left">Crypto Currency</TableHeadData>
-            <TableHeadData align="right">Quantity</TableHeadData>
-            <TableHeadData align="right">
+            <TableHeadData align='left'>Crypto Currency</TableHeadData>
+            <TableHeadData align='right'>Quantity</TableHeadData>
+            <TableHeadData align='right'>
               Cost Basis <span>(USD/BTC)</span>
             </TableHeadData>
-            <TableHeadData align="center">Actions</TableHeadData>
+            <TableHeadData align='center'>Actions</TableHeadData>
           </tr>
         </TableHead>
         <TableBody>
-          <TableRow>
-            <TableBodyData align="left">Bitcoin (BTC)</TableBodyData>
-            <TableBodyData align="right">2</TableBodyData>
-            <TableBodyData align="right">$100,000</TableBodyData>
-            <TableBodyData align="center">
-              <EditCoinIconButton />
-              <RemoveCoinIconButton onClick={onClickRemoveCoin} />
-            </TableBodyData>
-          </TableRow>
-          <TableRow>
-            <TableBodyData align="left">Dogecoin (DOGE)</TableBodyData>
-            <TableBodyData align="right">200</TableBodyData>
-            <TableBodyData align="right">$120</TableBodyData>
-            <TableBodyData align="center">
-              <EditCoinIconButton />
-              <RemoveCoinIconButton onClick={onClickRemoveCoin} />
-            </TableBodyData>
-          </TableRow>
+          {portfolioCoins.map((coin) => (
+            <TableRow>
+              <TableBodyData align='left'>
+                {coin.name_with_symbol}
+              </TableBodyData>
+              <TableBodyData align='right'>
+                {coin.bought_quantity}
+              </TableBodyData>
+              <TableBodyData align='right'>${coin.cost_basis}</TableBodyData>
+              <TableBodyData align='center'>
+                <EditCoinIconButton />
+                <RemoveCoinIconButton onClick={onClickRemoveCoin} />
+              </TableBodyData>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
 
       <BottomContainer>
-        <Button onClick={() => setFormStep("start")}>BACK</Button>
+        <Button onClick={() => setFormStep('start')}>BACK</Button>
         <Button
-          onClick={() => console.log("save")}
-          style={{ marginLeft: "10px" }}
-          primary="true"
+          onClick={() => console.log('save')}
+          style={{ marginLeft: '10px' }}
+          primary='true'
         >
           SAVE
         </Button>
--- src/components/PortfolioTable/PortfolioTable.tsx
+++ src/components/PortfolioTable/PortfolioTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 import {
   Wrapper,
@@ -10,8 +10,8 @@ import {
   Ticker,
   CoinImg,
   CoinNameWrapper,
-} from "../CryptoTable/CryptoTable.styled";
-import { PortfolioCoin } from "../../types/coins";
+} from '../CryptoTable/CryptoTable.styled';
+import { PortfolioCoin } from '../../types/coins';
 interface Props {
   coins: PortfolioCoin[];
 }
@@ -19,71 +19,67 @@ interface Props {
 const PortfolioTable: React.FC<Props> = ({ coins }) => {
   return (
     <Wrapper>
-      <Table cellSpacing="0" cellPadding="0">
+      <Table cellSpacing='0' cellPadding='0'>
         <thead>
           <TableHeadRow>
-            <TableHeadData align="center">Rank</TableHeadData>
-            <TableHeadData align="left">Name</TableHeadData>
-            <TableHeadData align="right">Quantity</TableHeadData>
-            <TableHeadData align="right">Price Paid (coin)</TableHeadData>
-            <TableHeadData align="right">Cost Basis</TableHeadData>
-            <TableHeadData align="right">Current Price (coin)</TableHeadData>
-            <TableHeadData align="right">Current Value</TableHeadData>
-            <TableHeadData align="right">24h %</TableHeadData>
+            <TableHeadData align='center'>Name</TableHeadData>
+            <TableHeadData align='right'>Quantity</TableHeadData>
+            <TableHeadData align='right'>Price Paid (coin)</TableHeadData>
+            <TableHeadData align='right'>Cost Basis</TableHeadData>
+            <TableHeadData align='right'>Current Price (coin)</TableHeadData>
+            <TableHeadData align='right'>Current Value</TableHeadData>
+            <TableHeadData align='right'>24h %</TableHeadData>
 
-            <TableHeadData align="right">Total Gain</TableHeadData>
+            <TableHeadData align='right'>Total Gain</TableHeadData>
           </TableHeadRow>
         </thead>
         <tbody>
           {coins.map((coin) => (
             <TableBodyRow key={coin.id}>
-              <TableBodyData align="center">
-                {coin.market_cap_rank}
-              </TableBodyData>
-              <TableBodyData align="left">
+              <TableBodyData align='left'>
                 <CoinNameWrapper>
                   <CoinImg src={coin.image} alt={coin.name} />
                   {coin.name} <Ticker>{coin.symbol}</Ticker>
                 </CoinNameWrapper>
               </TableBodyData>
-              <TableBodyData align="right">
+              <TableBodyData align='right'>
                 {coin.bought_quantity.toLocaleString()}
               </TableBodyData>
-              <TableBodyData align="right">
+              <TableBodyData align='right'>
                 $
                 {coin.bought_price_per_coin < 0
                   ? coin.bought_price_per_coin.toFixed(6)
                   : coin.bought_price_per_coin.toLocaleString()}
               </TableBodyData>
-              <TableBodyData align="right">
+              <TableBodyData align='right'>
                 ${coin.cost_basis.toLocaleString()}
               </TableBodyData>
-              <TableBodyData align="right">
+              <TableBodyData align='right'>
                 ${coin.current_price.toLocaleString()}
               </TableBodyData>
-              <TableBodyData align="right">
+              <TableBodyData align='right'>
                 ${coin.current_value.toLocaleString()}
               </TableBodyData>
               <TableBodyData
-                align="right"
+                align='right'
                 change={
                   coin.price_change_percentage_24h > 0
-                    ? "positive"
+                    ? 'positive'
                     : coin.price_change_percentage_24h < 0
-                    ? "negative"
-                    : ""
+                    ? 'negative'
+                    : ''
                 }
               >
                 {coin.price_change_percentage_24h.toFixed(2)}%
               </TableBodyData>
               <TableBodyData
-                align="right"
+                align='right'
                 change={
                   coin.total_gain_usd > 0
-                    ? "positive"
+                    ? 'positive'
                     : coin.total_gain_usd < 0
-                    ? "negative"
-                    : ""
+                    ? 'negative'
+                    : ''
                 }
               >
                 {coin.total_gain_usd < 0
