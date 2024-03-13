a6bc47a7c5c14154f5af1dff73ea1ff48764daf4
BayC <rachada_32@hotmail.com>
2024-03-13 01:24:31 -0400

    Format quotations

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { AppContainer } from "./App.styled";
-import NavBar from "./components/NavBar";
-import GlobalStyle from "./styles/globalStyles";
-import Home from "./pages/Home";
-import WatchList from "./pages/WatchList";
-import Portfolio from "./pages/Portfolio";
-import { WatchListProvider } from "./context/WatchList/WatchListContext";
-import { PortfolioProvider } from "./context/Portfolio/PortfolioContext";
-import { MarketCoinsProvider } from "./context/MarketCoins/MarketCoinsContext";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AppContainer } from './App.styled';
+import NavBar from './components/NavBar';
+import GlobalStyle from './styles/globalStyles';
+import Home from './pages/Home';
+import WatchList from './pages/WatchList';
+import Portfolio from './pages/Portfolio';
+import { WatchListProvider } from './context/WatchList/WatchListContext';
+import { PortfolioProvider } from './context/Portfolio/PortfolioContext';
+import { MarketCoinsProvider } from './context/MarketCoins/MarketCoinsContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
@@ -19,14 +19,14 @@ const App: React.FC = () => {
               <GlobalStyle />
               <NavBar />
               <Switch>
-                <Route path="/crypto-folio" exact component={Home} />
+                <Route path='/crypto-folio' exact component={Home} />
                 <Route
-                  path="/crypto-folio/watchlist"
+                  path='/crypto-folio/watchlist'
                   exact
                   component={WatchList}
                 />
                 <Route
-                  path="/crypto-folio/portfolio"
+                  path='/crypto-folio/portfolio'
                   exact
                   component={Portfolio}
                 />
